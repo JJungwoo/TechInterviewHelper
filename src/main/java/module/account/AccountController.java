57b6d3e01e9ebfa5856c5b2768928588bb7e60e5
jungwoo <jungwoo5759@gmail.com>
2021-09-16 15:28:15 +0900

    feat: accountcontroller 로직 추가

--- src/main/java/module/account/AccountController.java
+++ src/main/java/module/account/AccountController.java
@@ -2,4 +2,20 @@ package module.account;
 
 public class AccountController {
 
+    private static AccountController accountController = new AccountController();
+
+    private static AccountService accountService = AccountService.getInstance();
+
+    // 싱글톤
+    public static AccountController getInstance() {
+        return accountController;
+    }
+
+    public String userCreate(UserVO user) {
+        return accountService.save(user).getUserid();
+    }
+
+    public UserVO findByUserid(String userid) {
+        return accountService.findById(userid);
+    }
 }
